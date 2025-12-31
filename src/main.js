af0a15b0c896c3494fd94744f56dba49490cbee7
Rian <rianssouza02@gmail.com>
2025-12-31 15:03:18 -0300

    Adcionando faq e rodape

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -13,7 +14,18 @@ document.addEventListener('DOMContentLoaded', function(){
         })
 
     }
-})
+
+    for(let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
+});
+
+function abreOuFechaResposta(elemento){
+    const pergunta = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(pergunta);
+}
 
 function removeBotaoAtivo(){
     const buttons = document.querySelectorAll('[data-tab-button]')
@@ -24,7 +36,6 @@ function removeBotaoAtivo(){
     }
 
 }
-
 function escondeTodasAbas(){
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
